aaec549917a0bb838c8c86936a62aa92be6c6f14
Abdulrahman Ghazwani <d.m1999y@gmail.com>
2026-01-13 16:45:35 +0300

    Finish CRUD in service layer

--- src/app/notes/notes.service.ts
+++ src/app/notes/notes.service.ts
@@ -1,9 +1,37 @@
+import {HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Note } from './notes.module';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class NotesService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/notes';
+
+  constructor(private http: HttpClient) { }
+
+  getNotes(): Observable<Note[]> {
+    return this.http.get<Note[]>(this.apiUrl);
+  }
+
+  getNoteById(id: number): Observable<Note> {
+    return this.http.get<Note>('${this.apiUrl}/${id}');
+  }
+
+  createNote(note: Note): Observable<Note> {
+    return this.http.post<Note>(this.apiUrl, note);
+  }
+
+  updateNote(id: number, note:Note): Observable<Note>{
+    return this.http.put<Note>('${this.apiUrl}/${id}', note);
+  }
+
+  deleteNote(id: number): Observable<void> {
+    return this.http.delete<void>('${this.apiUrl}/${id}');
+  }
+
+
 }
